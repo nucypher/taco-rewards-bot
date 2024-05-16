1633a73af3bf86ccfb1d929438deecf60ea2d9c6
Manuel Montenegro <manuel@nucypher.com>
2024-05-17 01:03:24 +0200

    Add rewards calculation draft

--- scripts/rewards_bot.py
+++ scripts/rewards_bot.py
@@ -1,4 +1,5 @@
 import click
+import time
 from ape import project
 from ape.cli import ConnectedProviderCommand, account_option
 
@@ -8,10 +9,16 @@ from ape.cli import ConnectedProviderCommand, account_option
 def cli(account):
     account.set_autosign(enabled=True)
 
-    contract = project.dependencies["nucypher-contracts"]["main"].TACoApplication.at(
-        "0x329bc9Df0e45f360583374726ccaFF003264a136"
-    )
+    taco_application = project.dependencies["nucypher-contracts"][
+        "main"
+    ].TACoApplication.at("0x329bc9Df0e45f360583374726ccaFF003264a136")
 
-    push_rewards = 100  # TODO: calculate the rewards to be pushed
+    # TODO: calculate the rewards to be pushed
+    authorized = taco_application.authorizedOverall()
+    last_update_time = taco_application.lastUpdateTime()
+    time_elapsed = time.now() - last_update_time
+    # push_rewards = authorized * 0.0375 /
+
+    print(time_elapsed)
 
     # contract.pushReward(100, sender=account)
