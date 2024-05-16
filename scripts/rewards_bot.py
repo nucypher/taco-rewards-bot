7199daabfa041ba04394ca06dbdb86bc5366e2d1
Manuel Montenegro <manuel@nucypher.com>
2024-05-17 00:03:01 +0200

    Add signature to transaction call

--- scripts/rewards_bot.py
+++ scripts/rewards_bot.py
@@ -1,11 +1,17 @@
 import click
-from ape import project, Contract
+from ape import project
 from ape.cli import ConnectedProviderCommand, account_option
 
 
 @click.command(cls=ConnectedProviderCommand)
 @account_option()
 def cli(account):
+    account.set_autosign(enabled=True)
+
     contract = project.dependencies["nucypher-contracts"]["main"].TACoApplication.at(
         "0x329bc9Df0e45f360583374726ccaFF003264a136"
     )
+
+    push_rewards = 100  # TODO: calculate the rewards to be pushed
+
+    # contract.pushReward(100, sender=account)
