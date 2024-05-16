c60030ac9b14bc915cc57636814508c2dc9dfcb4
Manuel Montenegro <manuel@nucypher.com>
2024-05-16 23:26:24 +0200

    Import TACoApplication contract

--- scripts/rewards_bot.py
+++ scripts/rewards_bot.py
@@ -1,5 +1,11 @@
 import click
+from ape import project, Contract
+from ape.cli import ConnectedProviderCommand, account_option
 
-@click.command()
-def cli():
-    click.echo('Hello, World!')
+
+@click.command(cls=ConnectedProviderCommand)
+@account_option()
+def cli(account):
+    contract = project.dependencies["nucypher-contracts"]["main"].TACoApplication.at(
+        "0x329bc9Df0e45f360583374726ccaFF003264a136"
+    )
